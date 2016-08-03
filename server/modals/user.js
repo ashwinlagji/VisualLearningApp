568c17d816fe608b275bafe6b6a91918ca42e43f
ashwinlagji <ashwin_lagji@persistent.co.in>
2016-08-03 15:48:45 +0530

    folder structure ...

---
+++ server/modals/user.js
@@ -0,0 +1,25 @@
+//{
+//    _id: "any id"
+//    , title_of_tutorial: "insertion sort"
+//    , category: "mathematics |computer programming| science..."
+//    content: {
+//        video: "video_link"
+//        , gif: "gif_link"
+//        , text: "text"
+//    }
+//    , reviews: [{
+//            "name": "abc"
+//            , "comment": "bbsbcsdbcib sdcb "
+//            , "date": 1.455800194995e+12
+//        }
+//        
+//        , {
+//            "name": "abc"
+//            , "comment": "This is mym comment "
+//            , "date": 1.455800194995e+12
+//                 }]
+//
+//}
+//
+
+        
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//import mongodb package
+var mongodb = require("mongodb");
+
+//MongoDB connection URL - mongodb://host:port/dbName
+var dbHost = "mongodb://";
+
+//DB Object
+var dbObject;
+
+//get instance of MongoClient to establish connection
+var MongoClient = mongodb.MongoClient;
+
+//Connecting to the Mongodb instance.
+//Make sure your mongodb daemon mongod is running on port 27017 on localhost
+MongoClient.connect(dbHost, function (err, db) {
+    if (err) throw err;
+    console.log(err);
+    console.log("Succesfuly connected");
+    dbObject = db;
 });
+
+
+
 
 module.exports = router;
